748c67824f0617891a19d1baa66e69dfeae27ad5
Coders <coders@gmail.com>
2023-05-23 01:40:12 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Testimonial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Testimonial
+    {
+        public int Id { get; set; }
+        public string ClientName { get; set; }
+        public string ClientComment { get; set; }
+        public string ClientImage { get; set; }
+        public bool Status { get; set; }
+    }
+}
